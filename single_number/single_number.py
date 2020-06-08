e276e0e3bc193d90e5078796fb80e26d989f3a53
= <=>
2020-06-08 14:35:35 -0700

    finished single_number.py

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -2,10 +2,31 @@
 Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
+# implement a solution that finds the single number in _one_ pass through the input array with `O(1)` space complexity?
+
 def single_number(arr):
     # Your code here
+    cur = 0
+    found = 0
+    # if the two first elements are not the same.. then the list needs to be sorted
+    if arr[0] != arr[1]:
+        arr.sort()
 
-    pass
+    # while cur pointer is not the length of the array passed in... walk
+    while cur != len(arr) :
+        # print(f'checking... {arr[cur]} {arr[cur+1]}')
+
+        if arr[cur] == arr[cur + 1]:
+            pass
+        else:
+            found = cur
+            cur += 1
+
+        # update the cur pointer before the next iteration
+        cur += 2
+
+
+    return arr[found]
 
 
 if __name__ == '__main__':
